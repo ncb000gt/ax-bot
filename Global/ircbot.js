3e2190a4a97b1c80f86012e42ba87e18e66cb4da
Nick <nicholas.j.campbell@gmail.com>
2009-07-30 00:31:49 -0400

    'first_bot' created, initialized, connects, and logs into the server and joins the channel. Server and Channels are defined in an XML config that is e4x'd.

---
+++ Global/ircbot.js
@@ -0,0 +1,49 @@
+importPackage(Packages.org.jibble.pircbot);
+
+var ircbot = {
+    create: function(bot) {
+	// our own onMessage function
+	return new PircBot() {
+	    onMessage: function(channel, sender, login, hostname, message) {
+		if (channel in this.logChannels) {
+		    app.log('Found: ' +channel);
+		    app.log('Logging - ' + sender +': ' +message);
+		    var time = new Date().format('yyyyMMdd-hhmmss');
+		    var log = this.logChannels[channel];
+		    spp.log('logid: ' + log.id);
+		    log.content += ((!(log.content) || log.content == '')?'\n':'') +
+			time + ' ' + sender + ': ' + message;
+		}
+	    },
+	    logChannels: {},
+	    joinChannel: function(channel, key, log) {
+		var time = new Date().format('yyyyMMdd-hhmmss');
+		var server = this.getServer();
+		if (log) {
+		    app.log('Adding logging for ' +channel);
+		    var lb = this.bot.get(server+'-'+channel);
+		    if (!lb) {
+			lb = new LogBucket();
+			lb.server = server;
+			lb.channel = channel;
+			this.bot.add(lb);
+		    }
+		    var l = new Log();
+		    l.id = time;
+		    lb.add(l);
+
+		    this.logChannels[channel] = l;
+		}
+
+		/*app.log('key: -'+key+'-');
+		if (key && key != '') {
+		    app.log('using key');
+		    this.joinChannel(channel, key);
+		}*/
+
+		this.joinChannel('#'+channel);
+	    },
+	    bot: bot
+	}
+    }
+};
---
+++ Global/onstart.js
@@ -0,0 +1,42 @@
+function init() {
+    var hp = root.get('home');
+    if (!hp) {
+	hp = new Homepage();
+	hp.id = 'home';
+	hp.title = 'Logging, Trivia, Chat - IRC - AxBot Extraordinare';
+	hp.content = new XHTML('Chattery.');
+	root.add(hp);
+    }
+
+    var first_bot = hp.get('first_bot');
+    if (!first_bot) {
+	first_bot = new Bot();
+	first_bot.name = 'first_bot';
+	first_bot.password = 'test';
+	first_bot.servers = <>
+	    <servers>
+		<server domain="irc.freenode.net">
+		    <channel log="true">axiomstack</channel>
+		</server>
+	    </servers>
+	    </>;
+	hp.add(first_bot);
+    }
+
+    res.commit();
+
+    startBots();
+}
+
+function startBots() {
+    for each(var bot in app.getObjects('Bot')) {
+	var ibot = new ircbot.create(bot);
+	ibot.setName(bot.name);
+	for each (var server in bot.servers..server) {
+	    ibot.connect(server.@domain);
+	    for each(var channel in server..channel) {
+		ibot.joinChannel(channel.text(), channel.@key, (channel.@log == 'true'));
+	    }
+	}
+    }
+}
